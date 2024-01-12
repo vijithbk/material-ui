e9ef4d8d6032fbb0452665e85702156c5d6a23d7
Vijith <vijubalak@gmail.com>
2024-01-12 21:50:14 +0530

    Adding checkbox with form

--- mui-tutorials/src/TextFields.js
+++ mui-tutorials/src/TextFields.js
@@ -1,17 +1,47 @@
 import React, { useState } from 'react'
-import { TextField, Typography } from '@mui/material'
+import { TextField, Typography, Button, Checkbox, FormGroup, FormControlLabel } from '@mui/material'
 
 function TextFields() {
-    const [name, setName] = useState()
+    const [inputs, setInputs] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+    const handleChange = e => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(inputs)
+    }
   return (
     <div>
-        <TextField type='text' sx={{ margin: 3 }} placeholder='name' variant='outlined'
-            onChange={e => setName(e.target.value)}
-        />
-        <TextField type='email' sx={{ margin: 3 }} placeholder='Email' variant='standard' />
-        <TextField type='password' sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+        <form onSubmit={handleSubmit}>
+            <TextField type='text' sx={{ margin: 3 }} placeholder='name' variant='outlined'
+                name="name"
+                value={inputs.name}
+                onChange={handleChange}
+            />
+            <TextField type='email' sx={{ margin: 3 }} placeholder='Email' variant='standard'
+                name="email"
+                value={inputs.email}
+                onChange={handleChange}
+            />
+            <TextField type='password' sx={{ margin: 3 }} placeholder='Password' variant='filled'
+                name="password"
+                value={inputs.password}
+                onChange={handleChange}
+            />
 
-        <Typography sx={{margin: 3}} fontSize={20}>{name}</Typography>
+            <Typography sx={{margin: 3}} fontSize={20}>{inputs.name}</Typography>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+            </FormGroup>
+            <Button type='submit'>Submit</Button>
+        </form>
     </div>
   )
 }
